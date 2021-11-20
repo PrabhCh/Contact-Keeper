00b06cedc0a7ce3a71d25e1e4bfa305d8c36e4e3
Prabhpreet Chaudhary <Prabh@Prabhpreets-MacBook-Pro.local>
2021-11-19 20:14:00 -0500

    Auth and alert added

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,58 @@
+import React, { useReducer, useContext, useEffect } from 'react';
+import axios from 'axios';
+import AuthContext from './authContext';
+import authReducer from './authReducer';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types';
+
+// Action creators
+// NOTE: These could be moved to a separate file like in redux
+// but they remain here for ease of students transitioning
+
+// Load User
+
+// Register User
+
+// Login User
+
+// Logout
+
+// Clear Errors
+
+// AuthState Provider Component
+
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
